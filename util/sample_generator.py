060ce81ac0650916282f00f21dcd6a8639607cba
alex-witt <witt045@gmail.com>
2019-10-22 01:15:18 -0400

    Initial Procedural Generation success!

--- util/sample_generator.py
+++ util/sample_generator.py
@@ -5,6 +5,8 @@
 # procedural generation algorithm and use print_rooms()
 # to see the world.
 
+from random import randint
+
 
 class Room:
     def __init__(self, id, name, description, x, y):
@@ -34,14 +36,68 @@ class Room:
         Connect two rooms in the given n/s/e/w direction
         '''
         return getattr(self, f"{direction}_to")
-
+    def check_adjacent_rooms(self):
+        """
+        Checks each adjacent tile to see if there is a room there
+        """
 
 class World:
     def __init__(self):
         self.grid = None
         self.width = 0
         self.height = 0
-    def generate_rooms(self, size_x, size_y, num_rooms):
+        self.room_count = 0
+        self.rooms = []
+
+    def generate_blank_matrix(self, size_x, size_y):
+        # initialize the grid
+        self.grid = [None] * size_y
+        self.width = size_x
+        self.height = size_y 
+
+        for i in range( len(self.grid) ):
+            self.grid[i] = [None] * size_x
+        
+    def start_room(self, start_x, start_y):
+        start_room = Room(id=1, name='start_room', 
+                          description='The starting room',
+                          x=start_x, y=start_y)
+        self.grid[start_room.x][start_room.y] = start_room
+        self.room_count += 1
+
+        return start_room
+
+    def add_room(self,id, x, y):
+        """
+        Add a new room. Future version will specificy type of room
+        """
+        new_room = Room(id, name='new_room',
+                        description='An additional room', 
+                        x=x, y=y)
+        self.grid[x][y] = new_room
+        self.room_count += 1
+        return new_room
+
+    def generate_world(self, size_x, size_y, n_rooms):
+        """
+        Generates a map with n number rooms in x and y size
+        """
+        # Create Map
+        self.generate_blank_matrix(size_x=size_x, size_y=size_y)
+        # Generate Starting Location
+        start_x = randint(0,size_x,)
+        start_y = randint(0,start_y)
+        # Create Starting Room
+        prev_room = self.start_room()
+        selected_room = prev_room
+
+        while self.room_count < n_rooms:
+            roll = randint(0,3)
+            
+
+
+
+    def generate_zig_zag_rooms(self, size_x, size_y, num_rooms):
         '''
         Fill up the grid, bottom to top, in a zig-zag pattern
         '''
@@ -151,12 +207,47 @@ class World:
         print(str)
 
 
-w = World()
-num_rooms = 200
-width = 20
-height = 10
-w.generate_rooms(width, height, num_rooms)
-w.print_rooms()
+# w = World()
+# num_rooms = 200
+# width = 20
+# height = 10
+# w.generate_zig_zag_rooms(width, height, num_rooms)
+# w.print_rooms()
 
 
-print(f"\n\nWorld\n  height: {height}\n  width: {width},\n  num_rooms: {num_rooms}\n")
+# print(f"\n\nWorld\n  height: {height}\n  width: {width},\n  num_rooms: {num_rooms}\n")
+##########################################################
+
+# from util.sample_generator import *
+# w = World()
+# w.generate_blank_matrix(15,15)
+
+# x = randint(0,w.width)
+# y = randint(0,w.height)
+
+# selected_room = w.start_room(x,y)
+
+# w.print_rooms()
+
+##########################
+
+# y_val = selected_room.y
+# x_val = selected_room.x
+
+# roll = randint(0,3)
+
+# if roll == 0:
+#     x_val += 1
+# elif roll == 1:
+#     x_val -= 1
+# elif roll == 2:
+#     y_val += 1
+# else:
+#     y_val -=1
+    
+# new_room = w.add_room(w.room_count+1, x_val, y_val)
+
+# room_coords=[]
+# room_coords.append([new_room.x, new_room.y])
+
+# w.print_rooms()
